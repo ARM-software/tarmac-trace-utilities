9d0ad04b15296c930ce0ecfede089b846584ada7
Simon Tatham <simon.tatham@arm.com>
2023-02-03 09:55:31 +0000

    Add some stress tests of the btod module.
    I'm reminded of it by Arnaud's reorganisation last night, and now I
    look at it, I see that its list of test cases are all the really
    obvious things like min and max float values, or 1 plus or minus an
    ULP, or π.
    
    A technique has been well known for decades to find the FP values
    closest to a rounding boundary of the output decimal representation,
    so as to verify that the implementation correctly computes enough
    digits internally to round even those the right way. btod.cpp computes
    in exact big integers, so it ought to pass all those tests, but none
    of them were included in the test suite.
    
    This patch adds a selection of those values: the hardest rounding
    cases at both the minimum and maximum exponents (testing dealing with
    the largest intermediate integer values), and the hardest _overall_
    rounding cases (that is, over all possible exponents, the ones that
    differ from the rounding boundary by the least amount in ULP). Also,
    included some cases that _exactly_ hit the rounding boundary, testing
    the round-to-even tie breaking rule.

--- tools/btodtest.cpp
+++ tools/btodtest.cpp
@@ -54,6 +54,14 @@ int main(int argc, char *argv[])
     TEST(float_btod(0x3f800001), " 1.00000012e+00");
     TEST(float_btod(0x3f7fffff), " 9.99999940e-01");
     TEST(float_btod(0x40490fdb), " 3.14159274e+00");
+    TEST(float_btod(0x7f56cc86), " 2.85516737e+38");
+    TEST(float_btod(0x7f7e9e90), " 3.38447215e+38");
+    TEST(float_btod(0x00f1cc32), " 2.22056060e-38");
+    TEST(float_btod(0x00958f91), " 1.37349909e-38");
+    TEST(float_btod(0x2262aef2), " 3.07213267e-18");
+    TEST(float_btod(0x0cf38bfc), " 3.75243281e-31");
+    TEST(float_btod(0x39000000), " 1.22070312e-04");
+    TEST(float_btod(0x39c00000), " 3.66210938e-04");
     TEST(double_btod(0x7ff0000000000001ULL), " NaN");
     TEST(double_btod(0x7ff0000000000000ULL), " Inf");
     TEST(double_btod(0x7fefffffffffffffULL), " 1.7976931348623157e+308");
@@ -67,6 +75,16 @@ int main(int argc, char *argv[])
     TEST(double_btod(0x3ff0000000000001ULL), " 1.0000000000000002e+00");
     TEST(double_btod(0x3fefffffffffffffULL), " 9.9999999999999989e-01");
     TEST(double_btod(0x400921fb54442d18ULL), " 3.1415926535897931e+00");
+    TEST(double_btod(0x7fe001ae5cbef27aULL), " 8.9921547694833199e+307");
+    TEST(double_btod(0x7feeeb215a5b2125ULL), " 1.7369354705610752e+308");
+    TEST(double_btod(0x7fe119e9bbe1b649ULL), " 9.6071096028392074e+307");
+    TEST(double_btod(0x7fe56f15dd0f2856ULL), " 1.2041132521723314e+308");
+    TEST(double_btod(0x00103dfc78d23608ULL), " 2.2587466892102073e-308");
+    TEST(double_btod(0x001f1e779b0375b3ULL), " 4.3276313309456039e-308");
+    TEST(double_btod(0x4d73de005bd620dfULL), " 1.3076622631878654e+65");
+    TEST(double_btod(0x2b659a2783ce70abULL), " 1.2345501366327440e-99");
+    TEST(double_btod(0x3e60000000000000ULL), " 2.9802322387695312e-08");
+    TEST(double_btod(0x3e78000000000000ULL), " 8.9406967163085938e-08");
     cout << "pass " << pass << " fail " << fail << endl;
     return fail == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
 }
