ARM-software/tarmac-trace-utilities

commit 4b84b68bdd4099e91eb4c90f801a6fd7104da191

Add a new 'Reporter' abstraction.
This replaces all the previous pieces of code that would output
diagnostics and progress reports straight to standard error: fatal OS
errors, trace file parsing errors, reports on indexing progress and on
whether (and why) indexing was being performed at all.

Reporter itself is an abstract base class, and there's a global
unique_ptr<Reporter> variable which allows all parts of the code to
access the current application's single instance of it.

NFC in this commit: the only implementation of Reporter as yet is the
CommandLineReporter defined in lib/misc.cpp, which implements the
entire API by doing exactly the same things that I just replaced with
calls to that API.

lib/tarmacutil.cpp
@@ -19,6 +19,7 @@
 #include "libtarmac/tarmacutil.hh"
 #include "libtarmac/index.hh"
 #include "libtarmac/misc.hh"
+#include "libtarmac/reporter.hh"
 
 #include <iostream>
 #include <stdlib.h>
@@ -90,8 +91,8 @@ void TarmacUtility::postProcessOptions()
         bool is_big_endian = image->is_big_endian();
         if (bigend_explicit) {
             if (bigend != is_big_endian) {
-                warnx("Endianness mismatch between image and "
-                      "provided endianness");
+                reporter->warnx("Endianness mismatch between image and "
+                                "provided endianness");
             }
         } else {
             bigend = is_big_endian;
@@ -115,33 +116,28 @@ void TarmacUtilityBase::updateIndexIfNeeded(const TracePair &trace,
 {
     uint64_t trace_timestamp;
     if (!get_file_timestamp(trace.tarmac_filename, &trace_timestamp))
-        err(1, "%s: stat", trace.tarmac_filename.c_str());
+        reporter->err(1, "%s: stat", trace.tarmac_filename.c_str());
+
+    reporter->set_indexing_verbosity(verbose);
 
     if (doIndexing == Troolean::Auto) {
         uint64_t index_timestamp;
+        IndexUpdateCheck status;
+
         if (!get_file_timestamp(trace.index_filename, &index_timestamp)) {
-            if (verbose)
-                clog << "index file " << trace.index_filename
-                     << " does not exist; building it" << endl;
-            doIndexing = Troolean::Yes;
+            status = IndexUpdateCheck::Missing;
         } else if (index_timestamp < trace_timestamp) {
-            if (verbose)
-                clog << "index file " << trace.index_filename
-                     << " is older than trace file " << trace.tarmac_filename
-                     << "; rebuilding it" << endl;
-            doIndexing = Troolean::Yes;
+            status = IndexUpdateCheck::TooOld;
         } else if (!magic_number_ok(trace.index_filename)) {
-            if (verbose)
-                clog << "index file " << trace.index_filename
-                     << " was not generated by this version of the tool"
-                     << "; rebuilding it" << endl;
-            doIndexing = Troolean::Yes;
+            status = IndexUpdateCheck::WrongFormat;
         } else {
-            if (verbose)
-                clog << "index file " << trace.index_filename
-                     << " looks ok; not rebuilding it" << endl;
-            doIndexing = Troolean::No;
+            status = IndexUpdateCheck::OK;
         }
+
+        reporter->indexing_status(trace.index_filename, trace.tarmac_filename,
+                                  status);
+        doIndexing = (status == IndexUpdateCheck::OK ?
+                      Troolean::No : Troolean::Yes);
     }
 
     if (doIndexing == Troolean::Yes)

browser/curses.cpp
@@ -26,6 +26,7 @@
 #include "libtarmac/misc.hh"
 #include "libtarmac/parser.hh"
 #include "libtarmac/registers.hh"
+#include "libtarmac/reporter.hh"
 #include "libtarmac/tarmacutil.hh"
 
 #include <climits>
@@ -2123,6 +2124,8 @@ void run_browser(Browser &br)
 
     endwin();
 }
+
+std::unique_ptr<Reporter> reporter = make_cli_reporter();
 
 int main(int argc, char **argv)
 {

browser/wx.cpp
@@ -22,6 +22,7 @@
 
 #include "browse.hh"
 #include "libtarmac/argparse.hh"
+#include "libtarmac/reporter.hh"
 #include "libtarmac/tarmacutil.hh"
 
 #include <wx/wxprec.h>
@@ -296,13 +297,17 @@ struct Config {
             if (word == "font") {
                 font = line;
             } else if ((it = colour_kws.find(word)) != colour_kws.end()) {
-                if (!colours[it->second].parse(line))
-                    cerr << conf_path << ":" << lineno
-                         << ": unable to parse colour '" << line << "'" << endl;
+                if (!colours[it->second].parse(line)) {
+                    ostringstream oss;
+                    oss << conf_path << ":" << lineno
+                        << ": unable to parse colour '" << line << "'";
+                    reporter->warnx(oss.str().c_str());
+                }
             } else {
-                cerr << conf_path << ":" << lineno
-                     << ": unrecognised config directive '" << word << "'"
-                     << endl;
+                ostringstream oss;
+                oss << conf_path << ":" << lineno
+                    << ": unrecognised config directive '" << word << "'";
+                reporter->warnx(oss.str().c_str());
             }
         }
     }
@@ -3031,6 +3036,8 @@ wxIMPLEMENT_APP(GuiTarmacBrowserApp);
 wxFont TextViewWindow::font;
 int TextViewWindow::char_width;
 int TextViewWindow::baseline, TextViewWindow::line_height;
+
+std::unique_ptr<Reporter> reporter = make_cli_reporter();
 
 bool GuiTarmacBrowserApp::OnInit()
 {

include/libtarmac/misc.hh
@@ -72,13 +72,6 @@ struct TracePair {
     }
 };
 
-// Similar to the BSDish <err.h>: err and warn suffix strerror(errno)
-// to the message, whereas errx and warnx do not
-void err(int exitstatus, const char *fmt, ...);
-void errx(int exitstatus, const char *fmt, ...);
-void warn(const char *fmt, ...);
-void warnx(const char *fmt, ...);
-
 // In the per-platform source
 bool get_file_timestamp(const std::string &filename, uint64_t *out_timestamp);
 bool is_interactive();

include/libtarmac/reporter.hh
@@ -1,0 +1,100 @@
+/*
+ * Copyright 2021 Arm Limited. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * This file is part of Tarmac Trace Utilities
+ */
+
+#ifndef LIBTARMAC_REPORTER_HH
+#define LIBTARMAC_REPORTER_HH
+
+#include <memory>
+#include <ostream>
+#include <string>
+
+// Enumeration used by Reporter::indexing_status() below.
+enum class IndexUpdateCheck {
+    OK,             // no rebuild needed
+    Missing,        // rebuild needed: index not present
+    TooOld,         // rebuild needed: index older than trace file
+    WrongFormat,    // rebuild needed: index has wrong file format version
+};
+
+/*
+ * A Reporter is an object that knows how to display diagnostics to
+ * the user, and perhaps exit in the case where diagnostics are fatal.
+ *
+ * It's concealed behind an abstract base class so that the
+ * implementation can be swapped out between GUI and command-line
+ * tools. (On some GUI platforms, you can't print the progress of
+ * indexing to standard error, because you don't _have_ a standard
+ * error. So you have to do it with a GUI progress-bar dialog instead.
+ * Similarly for fatal error messages.)
+ */
+class Reporter {
+  protected:
+    bool verbose = false, progress = false;
+
+  public:
+    virtual ~Reporter() = default;
+
+    // Report warnings and fatal errors.
+    //
+    // Semantics are similar to the BSDish <err.h>. The err and errx
+    // functions are fatal errors, which terminate the program with
+    // the provided 'exitstatus' and do not return. warn and warnx
+    // print a message and continue.
+    //
+    // err and warn suffix strerror(errno) to the message, for
+    // reporting OS-level errors; errx and warnx do not.
+    virtual void err(int exitstatus, const char *fmt, ...) = 0;
+    virtual void errx(int exitstatus, const char *fmt, ...) = 0;
+    virtual void warn(const char *fmt, ...) = 0;
+    virtual void warnx(const char *fmt, ...) = 0;
+
+    void set_indexing_verbosity(bool val) { verbose = val; }
+
+    // Announce the status of an index update check (i.e. whether an
+    // index needs to be created, or whether it already exists and
+    // appears up to date).
+    virtual void indexing_status(const std::string &index_filename,
+                                 const std::string &trace_filename,
+                                 IndexUpdateCheck status) = 0;
+
+    // Report a warning or fatal error during indexing, such as a
+    // parsing problem. indexing_error does not return.
+    virtual void indexing_warning(const std::string &trace_filename,
+                                  unsigned lineno, const std::string &msg) = 0;
+    virtual void indexing_error(const std::string &trace_filename,
+                                unsigned lineno, const std::string &msg) = 0;
+
+    void set_indexing_progress(bool val) { progress = val; }
+
+    // Report progress during file indexing.
+    virtual void indexing_start(std::streampos total) = 0;
+    virtual void indexing_progress(std::streampos pos) = 0;
+    virtual void indexing_done() = 0;
+};
+
+std::unique_ptr<Reporter> make_cli_reporter();
+
+/*
+ * We don't expect different parts of the same process to need
+ * different Reporter classes, so there's no need to pass a Reporter
+ * pointer around all over the place. Instead, there's a single global
+ * instance, and each application initializes it before doing anything.
+ */
+extern std::unique_ptr<Reporter> reporter;
+
+#endif // LIBTARMAC_REPORTER_HH

lib/argparse.cpp
@@ -17,6 +17,7 @@
  */
 
 #include "libtarmac/argparse.hh"
+#include "libtarmac/reporter.hh"
 
 #include <cassert>
 #include <cstdlib>
@@ -253,9 +254,11 @@ void Argparse::parse(std::function<void(void)> final_validator)
         parse_or_throw();
         final_validator();
     } catch (ArgparseError e) {
-        clog << programname << ": " << e.msg() << endl
-             << string(programname.size() + 2, ' ') << "try '" << programname
-             << " --help' for help" << endl;
+        ostringstream oss;
+        oss << programname << ": " << e.msg() << endl
+            << string(programname.size() + 2, ' ') << "try '" << programname
+            << " --help' for help";
+        reporter->errx(1, "%s", oss.str().c_str());
         exit(1);
     } catch (ArgparseHelpAction) {
         help(cout);

lib/image.cpp
@@ -19,6 +19,7 @@
 #include "libtarmac/image.hh"
 #include "libtarmac/elf.hh"
 #include "libtarmac/misc.hh"
+#include "libtarmac/reporter.hh"
 
 #include <cstdio>
 #include <sstream>
@@ -171,7 +172,8 @@ Image::Image(const string &image_filename) : image_filename(image_filename)
 {
     elf_file = elf_open(image_filename);
     if (!elf_file)
-        errx(1, "Cannot open ELF file \"%s\"", image_filename.c_str());
+        reporter->errx(1, "Cannot open ELF file \"%s\"",
+                       image_filename.c_str());
     load_headers();
     load_symboltable();
 }

lib/index.cpp
@@ -20,6 +20,7 @@
 #include "libtarmac/misc.hh"
 #include "libtarmac/parser.hh"
 #include "libtarmac/registers.hh"
+#include "libtarmac/reporter.hh"
 
 #include <algorithm>
 #include <cassert>
@@ -881,8 +882,7 @@ void Index::got_event_common(TarmacEvent *event, bool is_instruction)
 
 bool Index::parse_warning(const string &msg)
 {
-    clog << tarmac_filename << ":" << lineno + lineno_offset << ": " << msg
-         << endl;
+    reporter->indexing_warning(tarmac_filename, lineno + lineno_offset, msg);
     return false;
 }
 
@@ -918,9 +918,6 @@ void Index::parse_tarmac_file(string tarmac_filename_, bool show_progress_meter)
     last_memroot = memroot;
     bypcroot = 0;
 
-    streampos filesize;
-    int progress_percentage = -1;
-
     /*
      * Read in the input.
      */
@@ -934,11 +931,9 @@ void Index::parse_tarmac_file(string tarmac_filename_, bool show_progress_meter)
     prev_lineno = lineno;
     curr_pc = KNOWN_INVALID_PC;
 
-    if (show_progress_meter) {
-        in.seekg(0, ios::end);
-        filesize = in.tellg();
-        in.seekg(0);
-    }
+    in.seekg(0, ios::end);
+    reporter->indexing_start(in.tellg());
+    in.seekg(0);
 
     TarmacLineParser parser(bigend, *this);
 
@@ -960,14 +955,7 @@ void Index::parse_tarmac_file(string tarmac_filename_, bool show_progress_meter)
 
         linepos = in.tellg();
 
-        if (show_progress_meter) {
-            int new_percentage = 100 * linepos / filesize;
-            if (new_percentage != progress_percentage) {
-                progress_percentage = new_percentage;
-                cout << "\rReading trace file (" << progress_percentage << "%";
-                cout.flush();
-            }
-        }
+        reporter->indexing_progress(linepos);
 
         if (!getline(in, line))
             break;
@@ -975,23 +963,20 @@ void Index::parse_tarmac_file(string tarmac_filename_, bool show_progress_meter)
         try {
             parser.parse(line);
         } catch (TarmacParseError e) {
-            clog << tarmac_filename << ":" << lineno << ": " << e.msg << endl;
             if (in.eof()) {
-                clog << "tarmac-browser: ignoring parse error on partial last"
-                        " line (trace truncated?)"
-                     << endl;
+                ostringstream oss;
+                oss << e.msg << endl << "tarmac-browser: ignoring parse error "
+                    "on partial last line (trace truncated?)";
+                reporter->indexing_warning(tarmac_filename, lineno, oss.str());
                 break;
             } else {
-                clog << endl;
                 remove(index_filename.c_str());
-                exit(1);
+                reporter->indexing_error(tarmac_filename, lineno, e.msg);
             }
         }
     }
 
-    if (show_progress_meter) {
-        cout << "\rReading trace file (finished)" << endl;
-    }
+    reporter->indexing_done();
 
     // Call got_event with no actual event, signalling end-of-file, so
     // that the last record is output (the same flushing of
@@ -1049,7 +1034,8 @@ IndexReader::IndexReader(const TracePair &trace)
 {
     MagicNumber &magic = *mmf.getptr<MagicNumber>(0);
     if (!magic.check())
-        errx(1, "%s: magic number did not match", index_filename.c_str());
+        reporter->errx(1, "%s: magic number did not match",
+                       index_filename.c_str());
     FileHeader &hdr = *mmf.getptr<FileHeader>(sizeof(MagicNumber));
     seqroot = hdr.seqroot;
     bypcroot = hdr.bypcroot;

commit db4723fda3cedf7d650ff3de5f8d429955a930ab

Make --show-progress-meter do something again.
It was apparently broken by commit 4b84b68bdd4099e (introducing the
Reporter abstraction), because that caused the progress-reporting code
in index.cpp to stop conditioning on its show_progress_meter
parameter, and instead just pass progress reports unconditionally to
the Reporter.

That makes sense, because in a GUI context the progress meter is
always shown, so index.cpp really does need to do that. But ever since
then, the command-line Reporter has been unconditionally _not_
printing a progress meter, because I forgot to put in the compensating
line of code in TarmacUtilityBase which uses the show_progress_meter
member variable to set the verbosity of the Reporter.

lib/tarmacutil.cpp
@@ -117,6 +117,7 @@ void TarmacUtilityBase::updateIndexIfNeeded(const TracePair &trace,
         reporter->err(1, "%s: stat", trace.tarmac_filename.c_str());
 
     reporter->set_indexing_verbosity(verbose);
+    reporter->set_indexing_progress(show_progress_meter);
 
     if (doIndexing == Troolean::Auto) {
         uint64_t index_timestamp;